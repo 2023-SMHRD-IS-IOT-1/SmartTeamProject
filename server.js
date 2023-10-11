fe8aaaa1565aaaca1fccfbfbdcb008cdcc8527cd
osh <ohseungheon3443@gmail.com>
2023-10-11 10:16:08 +0900

    서버 기본

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+
+const path = require('path');
+
+const bodyParser = require("body-parser");
+const nunjucks = require("nunjucks");
+
+const cors = require("cors");
+
+app.set("port", process.env.PORT || 3333)
+
+app.listen(app.get("port"),  (  ) => { 
+  console.log( app.get("port"), "번 포트에서 대기 중" );
+ } )
