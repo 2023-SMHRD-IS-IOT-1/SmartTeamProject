c66bed1e03eb988ed95eb5c5791c47bfadf42eb8
LDH <dlekgus0205@naver.com>
2023-10-11 15:12:23 +0900

    다현기본틀수정

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const path = require('path');
 
 const bodyParser = require("body-parser");
 const nunjucks = require("nunjucks");
+const session = require("express-session");
+const fileStore = require("session-file-store")(session);
+const indexRouter = require("./routes");
+const userRouter = require("./routes/user");
 
 const cors = require("cors");
 
-app.set("port", process.env.PORT || 3333)
+app.use(express.json());
+app.use(cors());
 
-app.listen(app.get("port"),  (  ) => { 
-  console.log( app.get("port"), "번 포트에서 대기 중" );
- } )
+app.set("port", process.env.PORT || 3333);
+
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(
+  session({
+    httpOnly: true,
+    resave: false,
+    secret: "secret",
+    store: new fileStore(),
+  })
+);
+
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port") + "번 포트에서 대기 중..");
+});
